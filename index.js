49cf5863027bbd84dd287eb0da8c72cd92e45d9e
TLEJ <tedriley@live.com>
2022-01-18 00:49:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -395,7 +395,7 @@ async function transcribe_witai(buffer) {
         console.log(output)
         stream.destroy()
         if (1)
-            return "Text: "+output.text;
+            return "Text: " + output.text + " Parse: " + JSON.parse(output.text);
         return output;
     } catch (e) { console.log('transcribe_witai 851:' + e); console.log(e) }
 }
